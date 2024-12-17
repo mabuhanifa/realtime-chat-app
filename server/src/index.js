673e95420ec9493890c34d50f174fdd301a969f7
mabuhanifa <moh.abuhanifa@gmail.com>
2024-12-17 18:05:22 +0600

    server initialized with npm commands

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const port = process.env.PORT || 5001;
+import * as dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+dotenv.config();
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
