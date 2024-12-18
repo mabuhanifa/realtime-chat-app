6e9c38d4f999becb82e9b6e439e70a30daea34ea
mabuhanifa <moh.abuhanifa@gmail.com>
2024-12-18 19:15:45 +0600

    signup api functioning

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,8 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
-
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 connectDB();
