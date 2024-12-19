aad0ea91040733f7ae7235f8687e72483c3714d4
mabuhanifa <moh.abuhanifa@gmail.com>
2024-12-19 18:16:05 +0600

    login controller and user token generation

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 5001;
 import * as dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 app.use(express.json());
