34a4011d3f7bafd990a557f27ff1b119b036540f
mabuhanifa <moh.abuhanifa@gmail.com>
2024-12-23 18:58:01 +0600

    cloudinary config and updateProfile api created

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   try {
@@ -78,13 +79,22 @@ export const logout = (req, res) => {
   }
 };
 
-export const updateProfile = (req, res) => {
+export const updateProfile = async (req, res) => {
   try {
-    console.log();
-
-    res.status(200).json({ message: "Updated Profile successfully" });
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("Error in logout controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 const port = process.env.PORT || 5001;
-import * as dotenv from "dotenv";
+import { config } from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-dotenv.config();
+
+config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
