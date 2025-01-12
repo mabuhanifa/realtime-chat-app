62fcb97dd1cf7c8ffa58e7b5b1aaec45d76ff7fd
mabuhanifa <moh.abuhanifa@gmail.com>
2025-01-13 04:16:13 +0600

    getUsersForSidebar controller created

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,17 @@
-import express from "express";
-const port = process.env.PORT || 5001;
-import { config } from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+import express from "express";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+const port = process.env.PORT || 5001;
 
 config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/messages", messageRoutes);
 connectDB();
 
 app.listen(port, () => {
