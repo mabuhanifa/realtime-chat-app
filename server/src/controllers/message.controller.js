3eb4ccedae2e565d632047aa415ceaf890a73ec4
mabuhanifa <moh.abuhanifa@gmail.com>
2025-01-23 23:56:03 +0600

    getMessages controller added

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -15,3 +15,22 @@ export const getUsersForSidebar = async (req, res) => {
       .json({ message: "Internal server error", error: error.message });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
