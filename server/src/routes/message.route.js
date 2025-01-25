94d4c9f4e5fa54aebe954fc181f0e161f2480850
mabuhanifa <moh.abuhanifa@gmail.com>
2025-01-25 23:21:10 +0600

    messages routes updated

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,8 +1,14 @@
 import express from "express";
+import {
+  getMessages,
+  getUsersForSidebar,
+} from "../controllers/message.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/users", authMiddleware);
+router.get("/users", authMiddleware, getUsersForSidebar);
+
+router.get("/:id", authMiddleware, getMessages);
 
 export default router;
